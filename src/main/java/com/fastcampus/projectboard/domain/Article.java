0974c12ef927b8f2afb52f4cf5c7ddf3874f0fb9
JJ Lee <maiload77@gmail.com>
2024-07-14 02:29:34 +0900

    #20 - 회원 계정 엔티티와 게시글, 댓글에 연관관계 적용

--- src/main/java/com/fastcampus/projectboard/domain/Article.java
+++ src/main/java/com/fastcampus/projectboard/domain/Article.java
@@ -8,7 +8,7 @@ import java.util.Objects;
 import java.util.Set;
 
 @Getter
-@ToString
+@ToString(callSuper = true)
 @Table(indexes = {
         @Index(columnList = "title"),
         @Index(columnList = "hashtag"),
@@ -22,29 +22,27 @@ public class Article extends AuditingFields{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Setter @ManyToOne(optional = false) private UserAccount userAccount; // 유저 정보 (ID)
+
     @Setter @Column(nullable = false) private String title;   // 제목
     @Setter @Column(nullable = false, length = 10000) private String content;     // 본문
 
     @Setter private String hashtag;    // 해시태그
 
-    @OrderBy("id")
+    @OrderBy("createdAt DESC")
     @ToString.Exclude
     @OneToMany(mappedBy = "article", cascade = CascadeType.ALL)
     private final Set<ArticleComment> articleComments = new LinkedHashSet<>();
 
-//    @CreatedDate @Column(nullable = false) private LocalDateTime createdAt;    // 생성일시
-//    @CreatedBy @Column(nullable = false, length = 100) private String createdBy;   // 생성자
-//    @LastModifiedDate @Column(nullable = false) private LocalDateTime modifiedAt;   // 수정일시
-//    @LastModifiedBy @Column(nullable = false, length = 100) private String modifiedBy;  // 수정자
-
-    private Article(String title, String content, String hashtag) {
+    private Article(UserAccount userAccount, String title, String content, String hashtag) {
+        this.userAccount = userAccount;
         this.title = title;
         this.content = content;
         this.hashtag = hashtag;
     }
 
-    public static Article of(String title, String content, String hashtag) {
-        return new Article(title, content, hashtag);
+    public static Article of(UserAccount userAccount, String title, String content, String hashtag) {
+        return new Article(userAccount, title, content, hashtag);
     }
 
     @Override
