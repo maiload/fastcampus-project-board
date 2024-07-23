4c0330b6da2aef10171aecd19c66c40bca757ba4
정진 <maiload77@gmail.com>
2024-07-23 18:10:01 +0900

    #47 - 도메인 변경점의 반영: 해시태그
    해시태그 엔티티가 추가되고, 게시글이 해시태그를 여러 개
    가질 수 있도록 설계가 변경됨.
    이 때문에 변경점이 크게 전파되었다.
    커밋이 컴파일 에러 상태로 두는 것은 좋지 않아서
    어쩔 수 없이 변경점을 모두 한 커밋에 담는데
    이 방법이 괜찮을지는 추후 더 고민해보자.
    
    그러나 도메인 변경만 반영하고 기능을 구현하지 않았으므로
    컴파일 에러만 잠시 피할 뿐, 테스트는 현재 실패함

--- src/main/java/com/fastcampus/projectboard/dto/request/ArticleCommentRequest.java
+++ src/main/java/com/fastcampus/projectboard/dto/request/ArticleCommentRequest.java
@@ -1,9 +1,15 @@
 package com.fastcampus.projectboard.dto.request;
 
 import com.fastcampus.projectboard.dto.ArticleCommentDto;
+import com.fastcampus.projectboard.dto.ArticleWithCommentsDto;
+import com.fastcampus.projectboard.dto.HashtagDto;
 import com.fastcampus.projectboard.dto.UserAccountDto;
+import com.fastcampus.projectboard.dto.response.ArticleWithCommentsResponse;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * DTO for {@link com.fastcampus.projectboard.domain.ArticleComment}
@@ -14,7 +20,6 @@ public record ArticleCommentRequest(Long articleId, String content) {
     return new ArticleCommentRequest(articleId, content);
   }
 
-
   public ArticleCommentDto toDto(UserAccountDto userAccountDto) {
     return ArticleCommentDto.of(
             articleId,
@@ -22,4 +27,5 @@ public record ArticleCommentRequest(Long articleId, String content) {
             content
     );
   }
+
 }
